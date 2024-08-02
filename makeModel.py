4ff9da877a4d21c8ff92ff5598eefefb104bd097
Tobyisepik <toby@stanislaus.co.uk>
2024-08-02 21:41:41 +0100

    adding the final touches to the actual model building (have not refined it yet)

--- makeModel.py
+++ makeModel.py
@@ -3,18 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
 from keras.api.datasets import mnist
 from keras.api.utils import to_categorical
-
 from keras.api.models import Sequential
-from keras.api.layers import Dense
+from keras.api.layers import Dense, Input
+
 
 def show_image(x_train, y_train, amount):
-  plt.figure(figsize=(10,8))
+  plt.figure(figsize=(10 ,8))
   for i in np.arange(1,amount):
       plt.subplot(int('33'+str(i)))
-      plt.imshow(x_train[i+10].reshape(28,28),cmap=plt.cm.binary)
+      plt.imshow(x_train[i+10].reshape(28, 28), cmap=plt.cm.binary)
       plt.title(y_train[i+10])
       plt.axis('off')
   plt.show()
@@ -31,9 +30,12 @@ num_train = 38000
 x_train,x_test,y_train,y_test = x[:num_train], x[num_train:], y[:num_train], y[num_train:]
 
 y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
 model = Sequential()
-model.add(Dense(10,activation='relu',input_shape=(x.shape[1],)))
+
+model.add(Input((x.shape[1],)))
+model.add(Dense(10,activation='relu'))
 model.add(Dense(10,activation='relu'))
 model.add(Dense(10,activation='relu'))
 model.add(Dense(10,activation='relu'))
@@ -43,4 +45,10 @@ model.compile(loss='categorical_crossentropy',
              optimizer='adam',
              metrics=['accuracy'])
 
+
 model.fit(x_train, y_train, epochs=10, verbose=2)
+
+
+model.evaluate(x_test, y_test)
+
+model.save('Number Reader.keras')
