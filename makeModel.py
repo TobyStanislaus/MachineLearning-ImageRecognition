34180bf587d523080b9b5f78eac241ef7a9ec888
Tobyisepik <toby@stanislaus.co.uk>
2024-08-02 21:17:05 +0100

    finished the making of the model part

--- makeModel.py
+++ makeModel.py
@@ -5,16 +5,42 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 from keras.api.datasets import mnist
+from keras.api.utils import to_categorical
+
+from keras.api.models import Sequential
+from keras.api.layers import Dense
+
+def show_image(x_train, y_train, amount):
+  plt.figure(figsize=(10,8))
+  for i in np.arange(1,amount):
+      plt.subplot(int('33'+str(i)))
+      plt.imshow(x_train[i+10].reshape(28,28),cmap=plt.cm.binary)
+      plt.title(y_train[i+10])
+      plt.axis('off')
+  plt.show()
 
 
 data = pd.read_csv('data\\train.csv')
 
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
-x, x = data.drop(['label'],axis=1).values, data['label']
+x, y = data.drop(['label'],axis=1).values, data['label']
 
-print(x.shape)
-print()
+# print(x.shape[1])
 
+num_train = 38000
+x_train,x_test,y_train,y_test = x[:num_train], x[num_train:], y[:num_train], y[num_train:]
 
+y_train = to_categorical(y_train)
 
+model = Sequential()
+model.add(Dense(10,activation='relu',input_shape=(x.shape[1],)))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='softmax'))
 
+model.compile(loss='categorical_crossentropy',
+             optimizer='adam',
+             metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, verbose=2)
